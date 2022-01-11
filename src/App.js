0769e4a4fe4a59932c260b90b56e4ab88b6d948a
Jay <7jay783@gmail.com>
2022-01-11 13:21:36 +0530

    Search by country completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React, {useEffect} from 'react';
 import './App.css';
-import DetailCard from './components/DetailCard'
-import SearchBar from './components/SearchBar';
-import {fetchData} from './api/index'
+
+import Covid from './components/Covid';
+
+
 
 function App() {
-  useEffect(() => {
-    const data =  fetchData();
-   console.log(data);
-  })
+  
   return (
     <div className="App">
-      <SearchBar/>
-     <DetailCard/> 
-     <DetailCard/> 
-
-    
+        <Covid/>
 
     </div>
   );
